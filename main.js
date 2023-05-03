f795f88cf87c6fb6aea56ef8bb8a9c2d0bd2bb3f
ulysse.guillot <ulysse.guillot@rtit-consulting.com>
2023-05-03 13:08:28 +0200

    deformation du planeMesh + light

--- main.js
+++ main.js
@@ -14,36 +14,47 @@ renderer.setSize(innerWidth, innerHeight)
 renderer.setPixelRatio(devicePixelRatio)
 document.body.appendChild(renderer.domElement)
 
-const boxGeometry = new THREE.BoxGeometry(
-    1,
-    1,
-    1
-)
-const boxMaterial = new THREE.MeshBasicMaterial(
-    {color: 0x00ff00}
-)
-const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial)
-
 const planeGeometry = new THREE.PlaneGeometry(
     5,
     5,
     10,
     10
 )
-const planeMaterial = new THREE.MeshBasicMaterial(
-    {color: 0xff0000}
-)
+const planeMaterial = new THREE.MeshPhongMaterial({
+    color: 0xff0000,
+    side: THREE.DoubleSide,
+    flatShading: true,
+    specular: 0x880088
+})
 const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial)
 
-scene.add(boxMesh)
+// Affichage des coord des vertices du planeMesh
+const {array} = planeMesh.geometry.attributes.position
+console.log(array)
+
+// Randomisation de la profondeur des vertices du planeMesh
+for (let i = 0; i < array.length; i += 3) {
+    const x = array[i]
+    const y = array[i + 1]
+    const z = array[i + 2]
+
+    array[i + 2] = z + Math.random()
+}
+
+
+const light = new THREE.DirectionalLight(
+    0xffffff,
+    1
+)
+light.position.set(0, 0, 1)
+
+scene.add(light)
 scene.add(planeMesh)
 camera.position.z = 5
 
 function animate() {
     requestAnimationFrame(animate)
     renderer.render(scene, camera)
-    boxMesh.rotation.x +=0.05
-    boxMesh.rotation.y +=0.02
 }
 
 animate()
